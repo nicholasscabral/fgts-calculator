2afc429c6c61be5930079989e2466165386c9abf
Nicholas Cabral <nicholasscabral@gmail.com>
2023-03-31 21:23:48 -0300

    feat: add navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,11 @@
+import styles from "../styles/navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <>
+      <div className={styles.navbar}>
+        <h1>Calculador de FGTS</h1>
+      </div>
+    </>
+  );
+}
