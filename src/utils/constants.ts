9e50fd7d7c5696835b220c2b16e4fbcaf71dd01c
Nicholas Cabral <nicholasscabral@gmail.com>
2023-04-14 20:00:42 -0300

    feat: add constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,25 @@
+import { SalaryFieldValues } from "@/types/calculator";
+import { EmploymentRelationship } from "@/types/employment-relationship";
+
+export const monthlyPercentageByEmploymentRelationship: Record<
+  keyof typeof EmploymentRelationship,
+  number
+> = {
+  [EmploymentRelationship.clt]: 0.08,
+  [EmploymentRelationship.learner]: 0.02,
+  [EmploymentRelationship.domestic]: 0.112,
+};
+
+export const displayNameByEmploymentRelationship: Record<
+  keyof typeof EmploymentRelationship,
+  string
+> = {
+  [EmploymentRelationship.clt]: "CLT",
+  [EmploymentRelationship.learner]: "Jovem aprendiz",
+  [EmploymentRelationship.domestic]: "Empregada(o) domestica",
+};
+
+export const emptySalaryFieldValues: SalaryFieldValues = {
+  income: 0,
+  monthsOfService: 0,
+};
